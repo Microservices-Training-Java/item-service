c4cb67047de147cff14a28630d805fa573d59bd3
anh.nguyen34 <anh.nguyen@eastgate-software.com>
2023-05-30 23:25:25 +0700

    feat: create hello API

--- src/main/java/org/aibles/item_service/controller/ItemFieldController.java
+++ src/main/java/org/aibles/item_service/controller/ItemFieldController.java
@@ -1,7 +1,10 @@
 package org.aibles.item_service.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/item-fields")
 public class ItemFieldController {
 
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public String greeting() {
+    return "Hello";
+  }
 }
