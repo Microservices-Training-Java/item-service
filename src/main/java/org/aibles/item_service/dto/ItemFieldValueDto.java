77e975994068245b44b8f35a7237a184052fb0bd
MerryGaming <nhoquenroi2002@gmail.com>
2023-06-13 13:15:54 +0700

    fix: fix validate fieldId and value

--- src/main/java/org/aibles/item_service/dto/ItemFieldValueDto.java
+++ src/main/java/org/aibles/item_service/dto/ItemFieldValueDto.java
@@ -1,5 +1,7 @@
 package org.aibles.item_service.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +9,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ItemFieldValueDto {
 
+  @NotNull(message = "Field Id cannot be null")
+  @Pattern(regexp = "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}", message = "Invalid Field Id")
   private String fieldId;
+  @NotNull(message = "Value cannot be null")
   private String value;
 }
